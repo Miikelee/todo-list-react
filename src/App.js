7017900756815371b0fc02cccc2368a26eee95f9
Michele <mikele.apc.1995@gmail.com>
2021-08-30 17:59:18 -0400

    todo-list-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [todos, setTodo] = useState([]);
+  const [value, setvalue] = useState("");
+
+  const addtodo = (e) => {
+    e.preventDefault();
+    setTodo([...todos, value]);
+    setvalue("");
+  };
+
+  const removetodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.splice(id, 1);
+    setTodo(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center"> Todo List with React</h1>
+      <div><form className="form">
+        <input className="input" value={value} placeholder="Write a todo" onChange={(e) => setvalue(e.target.value)} />
+        <button className="green" onClick={addtodo}>
+          add
+        </button>
+        <button className="red" onClick={() => setTodo([""])}>
+          clear
+        </button>
+      </form>
+      </div>
+      {todos.map((todo, id) => (
+        <div className="items">
+          <div className="item" key={id}>
+            <h4>{todo}</h4>
+            <button className="red" onClick={() => removetodo(id)}>
+              clear
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
-
-export default App;
+};
